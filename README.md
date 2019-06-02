4fe733e357664c6f9afe35a62495715af3b5e994
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2019-06-02 17:45:20 +0200

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # plg_user_backupslogs
-Joomla plugin that copies db entries of Akeeba Backup and Easy Joomla Backup into a tab separated csv file
+Joomla plugin that copies db entries of "Akeeba Backup" and "Easy Joomla Backup" into a tab separated csv file inside each backups folder. Runs only on login into backend.
---
+++ backupslogghsvs.php
@@ -0,0 +1,111 @@
+<?php
+defined('JPATH_BASE') or die;
+
+class PlgSystemBackupslogghsvs extends CMSPlugin
+{
+  protected $app;
+	protected $db;
+	private $execute = null;
+	protected $backuppaths = [];
+	protected $backuptables = [];
+
+	public function onAfterDispatch()
+	{
+		if (
+			!$this->app->isClient('administrator')
+			|| Factory::getDocument()->getType() !== 'html'
+		){
+			$this->execute = false;
+			return;
+		}
+		
+		$this->backuppaths = [
+			'akeeba' => JPATH_ADMINISTRATOR . '/components/com_akeeba/backup',
+			'ejb'    => JPATH_ADMINISTRATOR . '/components/com_easyjoomlabackup/backups'
+		];
+		
+		foreach ($this->backuppaths as $component => $path)
+		{
+			if (!is_dir($path) || !is_writable($path))
+			{
+				unset($this->backuppaths[$component]);
+			}
+		}
+		
+		if (!$this->backuppaths)
+		{
+			$this->execute = false;
+			return;
+		}
+
+		$this->backuptables = [
+			'akeeba' => 'ak_stats',
+			'ejb'    => 'easyjoomlabackup',
+		]
+
+		$prefix = $this->db->getPrefix();
+		
+		try
+		{
+			$tables = $this->db->getTableList();
+		}
+		catch (Exception $e)
+		{
+			$this->app->enqueueMessage('plg_system_backupslogghsvs could not get database table list. Code: ' . $e->getCode() . ' Message: ' . $e->getMessage());
+			$this->execute = false;
+			return;
+		}
+
+		foreach ($this->backuptables as $component => $table)
+		{
+			if (
+				!in_array($component, $this->backuppaths)
+				|| !in_array($prefix . $table, $tables) 
+			){
+				unset($this->backuptables[$component]);
+				continue;
+			}
+
+			$this->backuptables[$component] = $this->db->qn('#__' . $table);
+		}
+
+		if (!$this->backuptables)
+		{
+			$this->execute = false;
+			return;
+		}
+
+		$this->execute = true;
+	}
+	
+	public function onUserAfterLogin($options)
+	{
+		if ($this->execute !== true)
+		{
+			return;
+		}
+		
+		$query = $this->db->getQuery(true);
+		
+		$query->select('*');
+		
+		foreach ($this->backuptables as $component => $table)
+		{
+			$query->clear('from')->from($table);
+			$this->db->setQuery($query);
+		
+			try
+			{
+				$lines = $this->db->loadAssocList();
+				echo ' 4654sd48sa7d98sD81s8d71dsa ' . print_r($lines, true);exit;
+			}
+			catch (Exception $e)
+			{
+				$this->app->enqueueMessage('plg_system_backupslogghsvs could not get database table entries. Code: ' . $e->getCode() . ' Message: ' . $e->getMessage() . ' Table: ' . $e->$table());
+				$this->execute = false;
+				return;
+			}
+			
+		}
+	}
+}
